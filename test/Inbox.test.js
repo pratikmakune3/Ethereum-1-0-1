26abb908e834f946da160045b1f7c4ce887b54f4
Pratik Makune <pratikmakune3@users.noreply.github.com>
2018-04-17 20:54:38 +0530

    Writing tests for contract

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -1,10 +1,17 @@
 const assert = require('assert');
 const ganache = require('ganache-cli');
 const Web3 = require('web3'); // Web3 with capital 'W' as it's contructor function
-const web3 = new Web3(ganache.provider());
+
+/*
+	ganache.provider() is ganache specific provider. You can change this arg if network changes!
+*/ 
+
+const provider = ganache.provider()
+const web3 = new Web3(provider);
 const { interface, bytecode } = require('../compile')
 
-let accounts;
+let accounts, inbox;
+
 beforeEach(async () => {
 //STEP 1 : Get list of all ganache accounts	
 	
@@ -18,10 +25,22 @@ beforeEach(async () => {
 	accounts = await web3.eth.getAccounts();
 
 //STEP 2 : Use one of those accounts to deploy contract
+	inbox = await new web3.eth.Contract(JSON.parse(interface))
+		.deploy({ data: bytecode, arguments:['Hi there!']})
+		.send({ from: accounts[0], gas: '1000000' });
+
+	inbox.setProvider(provider);
 });
 
 describe('Inbox', () => {
 	it('deploys a contract', () => {
-		console.log(accounts);
+		// console.log(inbox);
+
+		/*
+			If the contract successfully deployed to n/w, inbox.options.address != null
+			Hence existance of inbox.options.address means 'inbox' contract successfully deployed!
+		*/ 
+		assert.ok(inbox.options.address);
+
 	});
 });
